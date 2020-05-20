01af8d50af78faf347cb52e5644b6a145e601112
Jeevika Wadhwa <jeevikaw17@gmail.com>
2020-05-20 22:01:45 +0530

    Calculates hypotenuse and area of right angled triangle

--- P04_RightTriangleSolverApp.py
+++ P04_RightTriangleSolverApp.py
@@ -5,9 +5,9 @@ print("Welcome to the Right Triangle Solver App\n")
 f = float(input("What is the first leg of the triangle: "))
 s = float(input("What is the second leg of the triangle: "))
 
-h = round(m.sqrt(f**2 + s**2), 3)
-a = round(0.5*f*s, 3)
+h = round(m.sqrt(f**2 + s**2), 3)	# hypotenuse
+a = round(0.5*f*s, 3)			# area
 
+# string formatting
 print(f"\nFor a triangle with legs of {f} and {s} the hypotenuse is {h}.")
-
 print(f"For a triangle with legs of {f} and {s} the area is {a}.")
