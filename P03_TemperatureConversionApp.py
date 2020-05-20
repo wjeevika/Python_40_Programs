38bc340d3d8709e0b8287394edf3934cd8e14ff6
Jeevika Wadhwa <jeevikaw17@gmail.com>
2020-05-20 21:58:42 +0530

    Converts Fahrenheit to Celsius and Kelvin

--- P03_TemperatureConversionApp.py
+++ P03_TemperatureConversionApp.py
@@ -2,9 +2,10 @@ print("Welcome to the Temperature Conversion App\n")
 
 f = float(input("What is the given temperature in degrees Fahrenheit: "))
 
-c = (f-32)*5/9
-k = c + 273.15
+c = (f-32)*5/9      # fahrenheit to celsius
+k = c + 273.15      # fahrenheit to kelvin
 
+# output
 print("\nDegrees Fahrenheit:\t", f)
 print("Degrees Celsius:\t", round(c, 4))
 print("Degrees Kelvin:\t\t", round(k, 4))
