beb1b4f1178af86598eab58826063567bd20082c
Jeevika Wadhwa <jeevikaw17@gmail.com>
2020-05-20 20:54:40 +0530

    Convert miles per hour to meter per second

---
+++ P02_MilesPerHourConversionApp.py
@@ -0,0 +1,8 @@
+print("Welcome to the Miles Per Hour Conversion App")
+
+s = float(input("What is your speed in miles per hour: "))
+# takes speed in miles per hour
+
+print("Your speed in meters per second is ", round(s*0.4474,2))
+# prints speed in meter per second, rounding off to two decimal places
+
