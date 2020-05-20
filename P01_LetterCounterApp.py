bf9a4e2707ff75b0c5d408a538ef1ba82366e9cb
Jeevika Wadhwa <jeevikaw17@gmail.com>
2020-05-20 22:04:12 +0530

    Searches a letter in the given sentence

--- P01_LetterCounterApp.py
+++ P01_LetterCounterApp.py
@@ -5,10 +5,9 @@ print(f"\nHello {s.capitalize()}!")
 
 print("I will count the number of times that a specific letter occurs in a message.")
 m = input("\nPlease enter a message: ")
-
 n = input("Which letter would you like to count the occurrences of: ")
 
-c=0
+c=0     
 for i in m.lower():
     if n.lower()==i:       # to make sure both upper and lower case are searched
         c+=1
